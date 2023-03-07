1cde5b243ee736ef7f29fcb41d9a6f207b9c490d
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-03-07 12:13:34 +0700

    add sort method

--- Algo3/Week3/Algo.cs
+++ Algo3/Week3/Algo.cs
@@ -6,3 +6,9 @@ List<int> originList = new List<int>();
 DataPreprocess.ReadData(ref originList, dataSet1);
 
 Console.WriteLine("Read data done!");
+
+int low = 0;
+int high = originList.Count()-1;
+DataPreprocess.Sort(ref originList, low, high);
+
+Console.WriteLine("Sorting done!");
--- Algo3/Week3/HuffmanCode.cs
+++ Algo3/Week3/HuffmanCode.cs
@@ -4,10 +4,12 @@ namespace HuffmanCode;
 //if it's a leaf node, value means its value
 public class tree {
     int value;
-    List<int> leftNodes;
-    List<int> rightNodes;
+    List<tree> leftNodes;
+    List<tree> rightNodes;
     public tree(int _value) {
         value = _value;
+        leftNodes = new List<tree>();
+        rightNodes = new List<tree>();
     }
 }
 
@@ -21,8 +23,37 @@ public static class DataPreprocess {
             }
         }
     }
+
+    public static void swap<T>(ref List<T> list, int one, int two) {
+        T temp = list[one];
+        list[one]= list[two];
+        list[two]= temp;
+    }
+
+    private static int partition(ref List<int> list, int low, int high) {
+        int pivot = list[high];
+
+        //i is index of the left element of pivot
+        int i = low-1;
+        for(int j=low; j<high-1; j++) {
+            if (list[j]<pivot) {
+                i++;
+                swap(ref list, i, j);
+            }
+        }
+        swap(ref list, i+1, high);
+        return i+1;  
+    }
+
+    public static void Sort(ref List<int> list, int low, int high) {
+        if(low<high) {
+            int pivot = partition(ref list, low, high);
+            Sort(ref list, pivot+1, high);
+            Sort(ref list, low, pivot-1);
+        }
+    }
 } 
 
-public static class HuffmanCode {
+public static class HuffmanMethod {
 
 }
