d15755c27e004b62fe9a5d37d6e95c4a48d01ddc
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-07-24 15:56:17 +0700

    add test.py and a DtoB func to algo.py

--- Algo4/Week2/Algo.py
+++ Algo4/Week2/Algo.py
@@ -8,7 +8,22 @@ def readGraph(inputFile):
     graph.close()
     return nV, coordinateList
 
+def DecToBin(num):
+    bin = ""
+    while num != 0:
+        if num % 2 == 0:
+            bin = "0" + bin
+        else: 
+            bin = "1" + bin
+        num = int(num / 2)
+    return bin
+
 input = "data_file.txt"
 nV, coordinateList = readGraph(input)
-print(coordinateList)
-print(nV)
+
+table = [[]]
+
+# for size in range (2, nV):
+#     for S in range (1, pow(2, nV-1), 2):
+        
+
