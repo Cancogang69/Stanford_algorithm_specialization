97217560c7cdc893ec7b302702f738b04f1bb356
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-07-29 16:56:18 +0700

    add distance and func functios

--- Algo4/Week3/Algo.py
+++ Algo4/Week3/Algo.py
@@ -3,11 +3,30 @@ def ReadGraph(inputFile):
         nV = int(graph.readline())
         coordList = []
         for v in range(nV):
-            coord = tuple(map(float, graph.readline().split(" ")))
+            coord = list(map(float, graph.readline().split(" ")))
+            coord[0] = int(coord[0])
             coordList.append(coord)
     return nV, coordList
 
+def distance(p1, p2):
+    return ((p1[0]-p2[0])**2 +
+            (p1[1]-p2[1])**2) ** 1/2
+
+def func(nV, coordList):
+    tourLenght = 0
+    curV = 1
+    for i in range(1, nV):
+        coordList[curV-1][0] = -1
+        minDis = INF
+        for v in coordList:
+            dis = distance(coordList[curV][1:3], v[1:3])
+            if dis < minDis:
+                minDis = dis
+                curV = v[0]
+        tourLenght += minDis
+    return tourLenght
+
+INF = float("inf")
 files = ["data_file.txt"]
 nV, coordList = ReadGraph(files[0])
 
-    
