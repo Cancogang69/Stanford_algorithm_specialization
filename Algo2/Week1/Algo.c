06c5cd783d34a5f8c31b99428e6de4c43008e76a
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-08-10 11:29:23 +0700

    add init1DArray func

--- Algo2/Week1/Algo.c
+++ Algo2/Week1/Algo.c
@@ -8,19 +8,20 @@ typedef struct vertex {
     int *list;
 } vertex;
 
-vertex* initGraph(int graphSize, const int initListSize) {
-    vertex *graph = malloc(sizeof *graph * graphSize);
-    if(graph==NULL) {
-        printf("cannot allocating memory for graph!");
+void* init1DArray(size_t typeSize, size_t arraySize) {
+    void *array = malloc(typeSize * arraySize);
+    if(array==NULL) {
+        printf("Cannot allocating memory!");
         exit(1);
     }
+    return array;
+}
+
+vertex* initGraph(int graphSize, const int initListSize) {
+    vertex *graph = init1DArray(sizeof *graph, graphSize);
 
     for(int i=0; i< graphSize; i++) {
-        graph[i].list = malloc(sizeof(int) * initListSize);
-        if(graph[i].list==NULL) {
-            printf("cannot allocating memory for vertex's list!");
-            exit(1);
-        }
+        graph[i].list = init1DArray(sizeof(int), initListSize);
         graph[i].used = 0;
         graph[i].size = initListSize;
     }
@@ -47,7 +48,7 @@ void readGraph(vertex *graph, vertex *revGraph, const char fileName[], size_t *g
     FILE *inputFile = fopen(fileName, "r");
     if(inputFile==NULL) {
         printf("Cannot read the input file!");
-        exit(0);
+        exit(1);
     }
 
     int largestV = 0;
@@ -74,14 +75,19 @@ void printGraph(vertex *graph, int nRows) {
     }
 }
 
-void KosarajuAlgo(vertex *graph, size_t graphSize) {
-    int finishTime = 0;
-    int leaderV = -1;
-    bool isExplored[876000] = {false};
-    for(int i=876000; i>0; i++) {
-
-    }
-
+void DFS(vertex *graph, int v, const int leaderV, int *fTime, 
+        bool *isExplored, int *fArray, int *leaderArray, int *fP) 
+{
+    isExplored[v] = true;
+    if(leaderV!=-1) 
+        leaderArray[leaderV]++;
+    vertex *node = &graph[v];
+    for(int i=0; i<node->used; i++)
+        if(isExplored[node->list[i]]==false)
+            DFS(graph, i, leaderV, fTime, isExplored, fArray, leaderArray, fP);
+    
+    (*fTime)++;
+    fArray[(*fP)++] = (*fTime);
 }
 
 int main() {
@@ -92,5 +98,19 @@ int main() {
     vertex *revGraph = initGraph(graphSize, initListSize);
     readGraph(graph, revGraph, input, &graphSize);
 
-    bool *flag = malloc(sizeof *flag * graphSize);
+    bool *isExplored = init1DArray(sizeof(bool), graphSize);
+    for(int i=0; i<graphSize; i++)
+        isExplored[i]=false;
+    int *fArray = init1DArray(sizeof(int), graphSize);
+    int fTime = -1;
+    int fPointer = 0;
+    for(int i=graphSize-1; i>=0; i--) {
+        if(isExplored[i]==false) {
+            DFS(graph, i, -1, &fTime, isExplored, fArray, NULL, &fPointer);
+        }
+    }
+    FILE *output = fopen("output.txt", "w");
+    for(int i=0; i<graphSize; i++) 
+        fprintf(output, "%d %d\n", i+1, fArray[i]);
+    fclose(output);
 }
