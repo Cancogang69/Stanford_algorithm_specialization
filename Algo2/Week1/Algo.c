8adf9f72b7d195d8a7e5190e10aed1eafb5d0f2a
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-08-11 17:17:46 +0700

    change vertex struct to node change a little bit in readGraph and addVertex func add some function, include: isEmpty, push, top, pop, initStack

--- Algo2/Week1/Algo.c
+++ Algo2/Week1/Algo.c
@@ -2,11 +2,84 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct vertex {
+typedef struct node {
     int size;
     int used;
     int *list;
-} vertex;
+} node;
+
+bool isEmpty(node list);
+void push(node *list, int value);
+int top(node stack);
+void pop(node *stack);
+void* init1DArray(size_t typeSize, size_t arraySize);
+node* initGraph(int graphSize, const int initListSize);
+node initStack();
+void addVertex(node *graph, const int sourceV, const int destinationV);
+void readGraph(node *graph, node *revGraph, const char fileName[], size_t *graphSize);
+void printGraph(node *graph, int nRows);
+void DFS(node *graph, int v, const int leaderV, int *fTime, 
+        bool *isExplored, int *fArray, int *leaderArray, int *fP);
+
+int main() {
+    char input[] = "data_file.txt";
+    const int initListSize = 10;
+    size_t graphSize = 876000;
+    node *graph = initGraph(graphSize, initListSize);
+    node *revGraph = initGraph(graphSize, initListSize);
+    readGraph(graph, revGraph, input, &graphSize);
+
+    printGraph(graph, 5);
+
+    // bool *isExplored = init1DArray(sizeof(bool), graphSize);
+    // for(int i=0; i<graphSize; i++)
+    //     isExplored[i]=false;
+    // int *fArray = init1DArray(sizeof(int), graphSize);
+    // int fTime = -1;
+    // int fPointer = 0;
+    // for(int i=graphSize-1; i>=0; i--) {
+    //     if(isExplored[i]==false) {
+    //         DFS(graph, i, -1, &fTime, isExplored, fArray, NULL, &fPointer);
+    //     }
+    // }
+    // FILE *output = fopen("output.txt", "w");
+    // for(int i=0; i<graphSize; i++) 
+    //     fprintf(output, "%d %d\n", i+1, fArray[i]);
+    // fclose(output);
+}
+
+void push(node *list, int value) {
+    if(list->used == list->size) {
+        list->size *= 2;
+        int *temp = realloc(list->list, sizeof(int) * list->size);
+        if(temp == NULL) {
+            printf("Cannot reallocating memory for vertex's list!");
+            exit(0);
+        }
+        list->list = temp;
+    }
+
+    list->list[list->used++] = value;
+}
+
+bool isEmpty(node stack) {
+    printf("stack is empty!");
+    return stack.used==0 ? true : false;
+}
+
+int top(node stack) {
+    if(isEmpty(stack)) {
+        printf("stack is empty!");
+        return -1;
+    }
+    return stack.list[stack.used-1]; 
+}
+
+void pop(node *stack) { 
+    if(isEmpty(*stack)) 
+        exit(1);
+    stack->used--;
+}
 
 void* init1DArray(size_t typeSize, size_t arraySize) {
     void *array = malloc(typeSize * arraySize);
@@ -17,8 +90,8 @@ void* init1DArray(size_t typeSize, size_t arraySize) {
     return array;
 }
 
-vertex* initGraph(int graphSize, const int initListSize) {
-    vertex *graph = init1DArray(sizeof *graph, graphSize);
+node* initGraph(int graphSize, const int initListSize) {
+    node *graph = init1DArray(sizeof *graph, graphSize);
 
     for(int i=0; i< graphSize; i++) {
         graph[i].list = init1DArray(sizeof(int), initListSize);
@@ -28,23 +101,20 @@ vertex* initGraph(int graphSize, const int initListSize) {
     return graph;
 }
 
-void addVertex(vertex *graph, const int sourceV, const int destinationV) {
-    vertex *v = &graph[sourceV];
-    if(v->used == v->size) {
-        v->size *= 2;
-        int *temp = realloc(v->list, sizeof(int) * v->size);
-        if(temp == NULL) {
-            printf("Cannot reallocating memory for vertex's list!");
-            exit(0);
-        }
-        v->list = temp;
-    }
+node initStack() {
+    node stack;
+    stack.size = 100;
+    stack.used = 0;
+    stack.list = init1DArray(sizeof(int), stack.size);
+    return stack;
+}
 
-    v->list[v->used++] = destinationV;
+void addVertex(node *graph, const int sourceV, const int destinationV) {
+    push(&graph[sourceV], destinationV);
 }
 
 // convert edge list to adjacency list
-void readGraph(vertex *graph, vertex *revGraph, const char fileName[], size_t *graphSize) {
+void readGraph(node *graph, node *revGraph, const char fileName[], size_t *graphSize) {
     FILE *inputFile = fopen(fileName, "r");
     if(inputFile==NULL) {
         printf("Cannot read the input file!");
@@ -63,10 +133,12 @@ void readGraph(vertex *graph, vertex *revGraph, const char fileName[], size_t *g
         addVertex(revGraph, destinationV-1, sourceV-1);
     }
 
-    *graphSize = largestV;
+    //update No. of vertices
+    (*graphSize) = largestV;
+    fclose(inputFile);
 }
 
-void printGraph(vertex *graph, int nRows) {
+void printGraph(node *graph, int nRows) {
     for(int i=0; i<nRows; i++) {
         int n = graph[i].used;
         printf("\nvertex %d, %d : ", i+1, n);
@@ -75,42 +147,23 @@ void printGraph(vertex *graph, int nRows) {
     }
 }
 
-void DFS(vertex *graph, int v, const int leaderV, int *fTime, 
+void DFS(node *graph, int v, const int leaderV, int *fTime, 
         bool *isExplored, int *fArray, int *leaderArray, int *fP) 
-{
-    isExplored[v] = true;
-    if(leaderV!=-1) 
-        leaderArray[leaderV]++;
-    vertex *node = &graph[v];
-    for(int i=0; i<node->used; i++)
-        if(isExplored[node->list[i]]==false)
-            DFS(graph, i, leaderV, fTime, isExplored, fArray, leaderArray, fP);
-    
-    (*fTime)++;
-    fArray[(*fP)++] = (*fTime);
-}
-
-int main() {
-    char input[] = "data_file.txt";
-    const int initListSize = 10;
-    size_t graphSize = 876000;
-    vertex *graph = initGraph(graphSize, initListSize);
-    vertex *revGraph = initGraph(graphSize, initListSize);
-    readGraph(graph, revGraph, input, &graphSize);
-
-    bool *isExplored = init1DArray(sizeof(bool), graphSize);
-    for(int i=0; i<graphSize; i++)
-        isExplored[i]=false;
-    int *fArray = init1DArray(sizeof(int), graphSize);
-    int fTime = -1;
-    int fPointer = 0;
-    for(int i=graphSize-1; i>=0; i--) {
-        if(isExplored[i]==false) {
-            DFS(graph, i, -1, &fTime, isExplored, fArray, NULL, &fPointer);
+{  
+    node stack = initStack();
+    push(&stack, v);
+    while(!isEmpty(stack)) {
+        int s = top(stack);
+        pop(&stack);
+        if(!isExplored[s]) {
+            isExplored[s] = true;
+            fArray[(*fP)++] = (*fTime)++;
+            if(leaderV!=-1) 
+                leaderArray[leaderV]++;
         }
-    }
-    FILE *output = fopen("output.txt", "w");
-    for(int i=0; i<graphSize; i++) 
-        fprintf(output, "%d %d\n", i+1, fArray[i]);
-    fclose(output);
+        node *vertex = &graph[v];
+        for(int i=0; i<vertex->used; i++)
+            if(!isExplored[vertex->list[i]])
+                push(&stack, vertex->list[i]);
+    }  
 }
