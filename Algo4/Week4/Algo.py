d6087ca5f8090e0cdd4a97a54a44e24b9fd3dd64
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-08-16 10:51:31 +0700

    add returnPos func change addGraph func

--- Algo4/Week4/Algo.py
+++ Algo4/Week4/Algo.py
@@ -1,12 +1,12 @@
-def addGraph(graph, revGraph, var, midPos):
-    #minusVar contain vertices for revGraph
-    minusVar = [-v for v in var]
-    var.extend(minusVar)
-    pos = [(midPos + v) for v in var]
+from collections import deque
+
+def returnPos(value, graphSize):
+    return int(graphSize/2) + value
+
+def addGraph(graph, var):
+    pos = [returnPos(v, len(graph)) for v in var]
     graph[pos[0]].append(var[1])
     graph[pos[1]].append(var[0])
-    revGraph[pos[2]].append(minusVar[1])
-    revGraph[pos[3]].append(minusVar[0])
 
 def readFile(fileName):
     with open(fileName) as inputFile:
@@ -16,14 +16,24 @@ def readFile(fileName):
         revGraph = [[] for _ in range(graphSize)]
         for line in inputFile:
             var = list(map(int, line.split(" ")))
-            addGraph(graph, revGraph, var, size)
+            minusVar = [-v for v in var]
+            addGraph(graph, var)
+            addGraph(revGraph, minusVar)
 
         return size, graph, revGraph
     
 def returnFileName(num):
     return "2sat" + str(num) + ".txt"
 
+
+def DFS(graph, v, isExplored, isCaptured, fArray, leaderArray):
+    stack = deque()
+    stack.append(v)
+    while(stack.count()!=0):
+        top = stack.pop()
+    
+
 size, graph, revGraph = readFile("2sat1.txt")
 
-print("done")
+
                 
---
+++ Algo4/Week4/testPlace.py
@@ -0,0 +1,3 @@
+num = 15.9
+intNum = int(num/2)
+print(intNum)
