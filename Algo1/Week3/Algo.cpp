a5a817b37cb2c4dd5d0fcbdad58ddd66a5cd6cba
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-09-04 17:10:10 +0700

    algo1 week3 completed!

--- Algo1/Week3/Algo.cpp
+++ Algo1/Week3/Algo.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
 
-void readData(std::string inputFile, int array[10000]) {
+#define size 10000
+
+void readData(std::string inputFile, int array[size]) {
     std::ifstream input(inputFile);
     if(!input) {
         std::cerr<<"Cannot open input file!\n";
@@ -14,10 +16,67 @@ void readData(std::string inputFile, int array[10000]) {
     input.close();
 }
 
+int findMedian(const int arr[size], int pPos[3]) {
+    int left = arr[pPos[0]],
+        right = arr[pPos[1]],
+        median = arr[pPos[2]],
+        bigger = left > right ? left : right,
+        smaller = left == bigger ? right : left;
+    median = bigger > median ? median : bigger;
+    median = smaller < median ? median : smaller;
+    for(int i=0; i<2; i++)
+        if(median==arr[pPos[i]])
+            pPos[2] = pPos[i];
+    return median;
+}
+
+void swap(int& num1, int& num2) {
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+int Partition(int array[size], int left, int right, int method) {
+    //pivot-choosing part
+    int pPos[] = {left, right, (left+right)/2};
+    int median = method == 2 ? findMedian(array, pPos) : 0;
+    int pivots[] = {array[pPos[0]], array[pPos[1]], median};
+    int pivot = pivots[method];
+    swap(array[pPos[0]], array[pPos[method]]);
+
+    //sorting part
+    int i = left + 1;
+    for(int j = left + 1; j<=right; j++) 
+        if(array[j] < pivot)
+            swap(array[j], array[i++]);
+    swap(array[pPos[0]], array[--i]);
+    return i;
+}
+
+void QuickSort(int array[size], int left, int right, int method, int &count) {
+    if(left>=right) return;
+
+    count += right-left;
+    int pPos = Partition(array, left, right, method);
+    QuickSort(array, left, pPos-1, method, count);
+    QuickSort(array, pPos+1, right, method, count);
+}
+
+void copyArray(const int array[size], int desArray[size]) {
+    for(int i=0; i<size; i++)
+        desArray[i] = array[i];
+}
+
 int main() {
     std::string inputFile = "data_file.txt";
-    int array[10000];
+    int array[size];
     readData(inputFile, array);
-    for(int i=0; i<10; i++)
-        std::cout<<array[i]<<" ";
+
+    for(int i=0; i<3; i++) {
+        int tempArray[size];
+        copyArray(array, tempArray);
+        int count = 0;
+        QuickSort(tempArray, 0, size-1, i, count);
+        std::cout<<count<<"\n";
+    }
 }
