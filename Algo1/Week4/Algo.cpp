0c641b04fa8887f73c8ffb4397f08e77bae59407
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-09-05 17:02:53 +0700

    add randomlyChoose func

--- Algo1/Week4/Algo.cpp
+++ Algo1/Week4/Algo.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
 
 #define graphSize 200
 
@@ -26,10 +28,16 @@ void readData(std::string inputFile, std::vector<int> graph[graphSize]) {
     input.close();
 }
 
+int randomlyChoose(std::vector<int> array) {
+    srand(time(NULL));
+    return array[rand() % array.size()];
+}
+
 int main() {
     std::string inputFile = "data_file.txt";
     std::vector<int> graph[graphSize];
     readData(inputFile, graph);
-    
+    std::vector<int> array = {1, 2, 3, 4, 7, 5, 6, 9};
+    std::cout<<"randomly choose a num in array: "<<randomlyChoose(array)<<"\n";
         
 }
