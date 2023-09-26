4a2791031b02db18c32e84f9e6b08118ed6ef5ef
Cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2023-09-26 17:21:46 +0700

    algo4 week1 completed!

--- Algo4/Week1/Algo.py
+++ Algo4/Week1/Algo.py
@@ -24,17 +24,17 @@ def BellmanFord(edgeList, nV):
 
 def reweight(edgeList, weightList):
     rwEdgeList = []
-    for edge in edgeList:
+    for i, edge in enumerate(edgeList):
         reweightLength = edge[2] + weightList[edge[0]-1] - weightList[edge[1]-1]
-        rwEdgeList.append([edge[0], edge[1], reweightLength])
-    return rwEdgeList
+        edgeList[i].append(reweightLength)
+    #return rwEdgeList
 
 def insertHeap(heap, value):
     heap.append(value)
     child = len(heap)-1
     parent = int((child-1)/2)
     while child!=parent:
-        if heap[child] > heap[parent]:
+        if heap[child][3] > heap[parent][3]:
             return
         heap[parent], heap[child] = heap[child], heap[parent]
         child = parent
@@ -49,43 +49,46 @@ def popHeap(heap):
     left = 1
     while left<len(heap):
         right = left+1 if left+1<len(heap) else left
-        minPos = left if heap[left]<heap[right] else right
-        if heap[parent] < heap[minPos]:
+        minPos = left if heap[left][3]<heap[right][3] else right
+        if heap[parent][3] < heap[minPos][3]:
             return
         heap[parent], heap[minPos] = heap[minPos], heap[parent]
         parent = minPos
         left = parent*2+1
 
 
-def Dijkstra(graph, sourceVertex, edgeList, rwEdgeList):
+def Dijkstra(graph, sourceVertex, edgeList):
     nV = len(graph)
     flagList = [False] * nV
+    heap = [[sourceVertex, sourceVertex, 0, 0], ]
     pathLengthList = [[0, 0]] * nV
-    heap = [[sourceVertex, sourceVertex, 0], ]
-
-    #min is the minimum path
-    #item pathLenghtList have 2 value:
-    #first value is the lenth of the minimum path
-    #second value is the reweight-length
+    pathLengthList[sourceVertex-1] = [0, 0]
     min = float('inf')
-    temp = nV
-    while(temp>0):
+    while(nV>0):
         edge = heap[0]
         popHeap(heap)
-        if(flagList[edge[1]-1]==True):
+        head = edge[1]-1
+        if flagList[head]:
             continue
-        temp -= 1
 
+        nV -= 1
+        flagList[head]=True
+        pathLengthList[head] = [edge[2], edge[3]]
+        if min>edge[2] and edge[0]!=edge[1]:
+            min = edge[2]
+        for index in graph[head]:
+            realLenght = edgeList[index][2] + pathLengthList[head][0]
+            reweightLenght = edgeList[index][3] + pathLengthList[head][1]
+            insertHeap(heap, [head+1, edgeList[index][1], realLenght, reweightLenght])
     return min
 
-def findShortestShortestPath(graph, edgeList, rwEdgeList):
-    nV = len(graph)
+def findShortestShortestPath(edgeList, nV):
     graph = [[] for v in range(nV)]
     for i, item in enumerate(edgeList):
         graph[item[0]-1].append(i)
     min = float('inf')
     for i in range(1, nV+1):
-        localMin = Dijkstra(graph, i, edgeList, rwEdgeList)
+        localMin = Dijkstra(graph, i, edgeList)
         if(localMin<min):
             min = localMin
     return min
@@ -102,8 +105,9 @@ if __name__ =="__main__":
             print("NULL")
             continue
 
-        rwEdgeList = reweight(edgeList, weightList)
+        #add reweighting lenght to edgeList
+        reweight(edgeList, weightList)
 
         #find shortest shortest path in graph
-        min = findShortestShortestPath(edgeList, rwEdgeList)
+        min = findShortestShortestPath(edgeList, nV)
         print(min)
